acbc1b949b66d504d25c680718b4023c43c32d00
qintb <qintb@hua-cloud.com.cn>
2018-09-12 18:30:45 +0800

    新增主题管理

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +12,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  title = '后台管理系统';
-  
-  constructor() { }
+  validateForm: FormGroup;
 
-  ngOnInit() {
+  submitForm(): void {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[ i ].markAsDirty();
+      this.validateForm.controls[ i ].updateValueAndValidity();
+    }
+  }
+  
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      userName: [ null, [ Validators.required ] ],
+      password: [ null, [ Validators.required ] ]
+    });
   }
 
 }
