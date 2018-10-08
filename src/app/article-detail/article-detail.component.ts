d0bb7807330bf1ea9c6a8b99e2312287d570a6ba
qintengbo <qintb@hua-cloud.com.cn>
2018-10-09 01:06:11 +0800

    新增md文件预览

--- src/app/article-detail/article-detail.component.ts
+++ src/app/article-detail/article-detail.component.ts
@@ -15,10 +15,26 @@ import {
 export class ArticleDetailComponent implements OnInit {
   breadcrumbText: string; // 面包屑最后一级标题
   validateForm: FormGroup;
+  previewState = false; // 预览状态
+  markdown = '# H1';
 
   constructor(
     private fb: FormBuilder
   ) { }
+
+  // 提交表单
+  submitForm = ($event, value) => {
+    $event.preventDefault();
+    for (const i of Object.keys(this.validateForm.controls)) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+    console.log(value);
+  }
+  // 预览md文件
+  preview(): void {
+    this.previewState = !this.previewState;
+  }
 
   ngOnInit() {
     this.validateForm = this.fb.group({
