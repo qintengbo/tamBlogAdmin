604ec4f040976a54f97db9a60ff4d17e68485b7d
qintb <qintb@hua-cloud.com.cn>
2019-05-07 19:15:52 +0800

    升级angular/cli和ng-zorrro-antd版本

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { NzMessageModule, NzInputModule, NzButtonModule, NzFormModule, NzIconModule } from 'ng-zorro-antd';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from '@/login/login.component';
 import { LoginRoutingModule } from '@/login/login-routing.module';
@@ -9,7 +9,11 @@ import { LoginRoutingModule } from '@/login/login-routing.module';
   imports: [
     CommonModule,
     FormsModule,
-    NgZorroAntdModule,
+    NzInputModule,
+    NzMessageModule,
+    NzButtonModule,
+    NzFormModule,
+    NzIconModule,
     ReactiveFormsModule,
     LoginRoutingModule
   ],
