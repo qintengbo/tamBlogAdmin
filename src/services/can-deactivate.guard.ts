dfa279f517d6fe1df14175f232660358591c56a8
qintb <qintb@hua-cloud.com.cn>
2019-05-08 18:43:48 +0800

    优化代码

--- src/services/can-deactivate.guard.ts
+++ src/services/can-deactivate.guard.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 
 export interface CanComponentDeactivate {
   canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
- }
+}
 
 @Injectable({
   providedIn: 'root'
